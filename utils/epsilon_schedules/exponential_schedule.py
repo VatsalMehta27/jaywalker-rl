cf93006a0525875ea47d1c6573a330bd777434d0
Vatsal Mehta <vatsalbmehta3@gmail.com>
2024-11-15 01:05:23 -0500

    abstracted epsilon schedulers

--- utils/epsilon_schedules/exponential_schedule.py
+++ utils/epsilon_schedules/exponential_schedule.py
@@ -1,24 +1,16 @@
 import numpy as np
 
+from utils.epsilon_schedules.epsilon_schedule import EpsilonSchedule
 
-class ExponentialSchedule:
+
+class ExponentialSchedule(EpsilonSchedule):
     def __init__(self, start_value, end_value, duration):
-        """Exponential schedule from `value_from` to `value_to` in `num_steps` steps.
-
-        $value(t) = a * exp(bt)$
-
-        :param value_from: Initial value
-        :param value_to: Final value
-        :param num_steps: Number of steps for the exponential schedule
-        """
-        self.start_value = start_value
-        self.end_value = end_value
-        self.duration = duration
+        super().__init__(start_value, end_value, duration)
 
         self.a = self.start_value
         self.b = (1 / (self.duration - 1)) * np.log(self.end_value / self.start_value)
 
-    def value(self, step) -> float:
+    def get_value(self, step: int) -> float:
         """Return exponentially interpolated value between `value_from` and `value_to`interpolated value between.
 
         Returns {
