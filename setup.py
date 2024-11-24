7a51b0c10a3c722ce76f9546cbe2007a2a58b9b6
Vatsal Mehta <vatsalbmehta3@gmail.com>
2024-11-24 12:00:16 -0500

    added to make src installable

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jaywalker-rl",
+    version="0.1",
+    description="Jaywalker Reinforcement Learning Project",
+    url="https://github.com/VatsalMehta27/jaywalker-rl",
+    packages=find_packages(),
+    python_requires=">=3.12",
+    install_requires=[
+        "numpy",
+        "gymnasium",
+        "matplotlib",
+        "pre-commit",
+        "torch",
+        "opencv-python",
+        "tqdm",
+        "setuptools",
+    ],
+)
